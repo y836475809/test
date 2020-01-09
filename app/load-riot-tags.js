f1d01bfb28b867554a3e09871da3367ce9bab583
y836475809 <tttdaytttday@gmail.com>
2020-01-10 01:26:02 +0900

    load tag by function

--- app/load-riot-tags.js
+++ app/load-riot-tags.js
@@ -1,6 +1,35 @@
 
+const load_rtags = (rtags, dir) => {
+    rtags.map(value => {
+        return `${dir}/${value}`;
+    }).forEach(value => {
+        const script = document.createElement("script");
+        script.type = "riot/tag";
+        script.src = value;
+        document.body.appendChild(script);
+    });
+};
+
+const load_main_rtags = (dir) => {
+    const rtags = [
+        "main-page.tag",
+        "accordion.tag",
+        "pagination.tag",
+        "modal-dialog.tag",
+        "library-page.tag",
+        "search-page.tag",
+        "download-schedule-dialog.tag",
+        "download-page.tag",
+        "play-history.tag",
+        "setting-page.tag",
+        "mylist-page.tag",
+        "bookmark-page.tag"
+    ];
+    load_rtags(rtags, dir);
+};
+
 const load_player_rtags = (dir) => {
-    [
+    const rtags = [
         "player-main-page.tag",
         "modal-dialog.tag",
         "player-tags.tag",
@@ -14,15 +43,11 @@ const load_player_rtags = (dir) => {
         "comment-ng-setting.tag",
         "comment-display-setting.tag",
         "open-video-form.tag"
-    ].map(value => {
-        return `${dir}/${value}`;
-    }).forEach(value => {
-        const script = document.createElement("script");
-        script.type = "riot/tag";
-        script.src = value;
-        document.body.appendChild(script);
-    });
+    ];
+    load_rtags(rtags, dir);
 };
+
 module.exports = {
+    load_main_rtags,
     load_player_rtags,
 };
